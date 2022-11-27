9f4b363f1b71ea9803f5d44876a69bdf8f23271a
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2022-11-27 10:30:45 +0530

    Update Lecture18-variables-CiperSchools.py

--- Lecture18-variables-CiperSchools.py
+++ Lecture18-variables-CiperSchools.py
@@ -2,9 +2,9 @@ number1=2
 print(number1)
 number1=4
 print(number1)
-useronename= "sunny"
+useronename= "Tushar singh"
 print(useronename)
-name,age="Sunny","20"
+name,age="tushar singh","20"
 print("hello " + name + " your age is "+ age)
 x=y=z=3
 print(x+y+z)
