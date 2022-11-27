2e9da0fce960ac3093315ecf7127470c024b4ca5
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2022-11-27 10:37:55 +0530

    Update Lecture36-solve problems with space using strips method in python-CiperSchools.py

--- Lecture36-solve problems with space using strips method in python-CiperSchools.py
+++ Lecture36-solve problems with space using strips method in python-CiperSchools.py
@@ -1,4 +1,7 @@
-name = "Sunny"
-dots = ".........."
-print(name + dots)
-print(name.lstrip() + dots)
+name= "tushar chauhan"
+dots="................"
+print(name+dots)
+print(name.lstrip()+dots)
+print(name.rstrip()+dots)
+print(name.strip()+dots)
+print(name.replace(" ", "")+dots)
