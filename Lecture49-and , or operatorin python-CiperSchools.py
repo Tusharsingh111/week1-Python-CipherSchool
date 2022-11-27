404728e0f2057c858f4c8d9bc6306d678d6269e4
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2022-11-27 10:46:37 +0530

    Update Lecture49-and , or operatorin python-CiperSchools.py

--- Lecture49-and , or operatorin python-CiperSchools.py
+++ Lecture49-and , or operatorin python-CiperSchools.py
@@ -1,23 +1,23 @@
-name = "Sumit"
+name = "Tushar"
 age = 19
-if name=="Sumit" and age==19:
+if name=="Tushar" and age==19:
     print("condition  true")
 else:
     print("condition false")
 
 
-if name=="Sumit" and age==20:
+if name=="Nishant" and age==20:
      print("condition  true")
 else:
     print("condition false")
 
-if name=="Sunny" or age==19:
+if name=="Tushar" or age==19:
      print("condition  true")
 else:
     print("condition false")
 
 
-if name=="Sunny" or age==20:
+if name=="Tushar" or age==20:
      print("condition  true")
 else:
     print("condition false")
